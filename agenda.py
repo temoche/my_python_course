9a525d5556f3b0e300b762eee144a5967d3b3a47
Israel <israel.temoche.l@uni.pe>
2020-01-23 18:08:11 -0500

    editando la agenda

--- agenda.py
+++ agenda.py
@@ -1,7 +1,7 @@
 #Diccionario que hace de agenda
 dic = {
-'Anthony':['Luzquiños', '936962826', 'sluzquinosa@uni.pe']
-'Israel': ['temoche', '94535928','israel.temoche.l@uni.pe']
+'Anthony':['Luzquiños', '936962826', 'sluzquinosa@uni.pe'],
+'Israel' :['temoche', '94535928','israel.temoche.l@uni.pe']
 }
 def mostrar_contactos ():
     print ('Contactos: ')
@@ -9,8 +9,19 @@ def mostrar_contactos ():
         print(position + 1, key, dic[key])
 
 def agregar_contacto (nombre):
-    pass
+    dic ['nombre'] = 'apellido'
+    print (dic)
+    
 def buscar_contacto(nombre):
-    pass
-def eliminar_contacto():
-    pass
+    if nombre in dic:
+        print ('Contacto encontrado: ')
+        print(nombre, dic[nombre])
+    else:
+        print('No existe el contacto')
+    print()
+def eliminar_contacto(nombre):
+    dic.pop(nombre)
+    mostrar_contactos()
+ #actividad.
+ # Generar un menú con las opciones antes programadas y que termine al ingresar "0".                
+
--- changelog.md
+++ changelog.md
@@ -1,3 +1,6 @@
 # Versiones
 ## version 1.0.1
 ### Se añade todos los archivos atrasados
+## version 1.0.2
+### se añaden cambios al programa "agenda.py"
+
